3ebbdd2f50045522d81eebba477445d78d99965a
AlmeidaCayo <cayolucasalmeida12@gmail.com>
2024-06-17 21:06:00 -0300

    Criação tabelas de endereço

---
+++ script-criacao-endereco.sql
@@ -0,0 +1,26 @@
+create table estados(
+	idEstado int primary key,
+    uf varchar(4),
+    nome varchar(20)
+);
+
+create table cep(
+	idCep int primary key not null auto_increment,
+    cep varchar(15),
+    logradouro varchar(255),
+    complemento varchar(100),
+    bairro varchar(100),
+    idMunicipio int not null
+);
+
+create table municipios(
+	idMunicipio int primary key not null auto_increment,
+    nome varchar(100),
+    idEstado int not null
+);
+
+ALTER TABLE municipios
+ADD CONSTRAINT idEstado FOREIGN KEY (idEstado) REFERENCES estados(idEstado);
+
+alter table cep
+add constraint idMunicipio foreign key(idMunicipio) references municipios(idMunicipio);
